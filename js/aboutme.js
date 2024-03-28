fe85e0bd7e76d5c4759d6a5a9dce03d29c481069
Norkindon <162091513+Norkindon@users.noreply.github.com>
2024-03-28 22:36:09 +0400

    update

--- js/aboutme.js
+++ js/aboutme.js
@@ -1,7 +1,7 @@
 window.onload = function() {
     var textContainer = document.getElementById('text-container');
 
-    fetch('./assets/text/aboutme.txt')
+    fetch('../assets/text/aboutme.txt')
         .then(response => response.text())
         .then(data => {
             textContainer.textContent = data; // Set the text content of the container
